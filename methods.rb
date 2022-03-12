4fff7d105e9c4d13787f6326b9cb4a8f0c37c4b0
wwwestin <westin.h@gmail.com>
2022-03-12 17:20:49 -0500

    Completed code-along

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  def add(num1, num2)
+    num1 + num2
+  end
+  
+  def halve(num)
+    return nil unless num.class == Integer
+    
+    num / 2
+  end
+
+  greet_programmer() 
+  greet "John"
+  greet_with_default()
+  puts add(2,4)
+  puts halve(4)
+  
